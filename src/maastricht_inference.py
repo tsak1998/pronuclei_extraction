91b9792333b9726b4fae935906b397131e3fbe43
tsak1998 <tsakalis.pantelis@gmail.com>
2025-10-06 21:31:15 +0300

    fix

--- src/maastricht_inference.py
+++ src/maastricht_inference.py
@@ -406,11 +406,11 @@ def inference_whole_slide(model, slide_pth: Path, max_frame: int):
         image_pn_isolated = image_ar.copy()
         # image_pn_isolated[:, ~upscaled_mask1.astype(bool)] = 0
         # isolated_pns.append(image_pn_isolated.transpose(1, 2, 0))
-        image_ar[0, upscaled_mask1.astype(bool)] = 1
-        image_ar[1, upscaled_mask2.astype(bool)] = 1
+        # image_ar[0, upscaled_mask1.astype(bool)] = 1
+        # image_ar[1, upscaled_mask2.astype(bool)] = 1
         # image_ar[2, upscaled_mask3.astype(bool)] = 1
 
-        final_images.append(Image.fromarray(image_ar.transpose(1, 2, 0)))
+        # final_images.append(Image.fromarray(image_ar.transpose(1, 2, 0)))
 
     return (
         final_images,
